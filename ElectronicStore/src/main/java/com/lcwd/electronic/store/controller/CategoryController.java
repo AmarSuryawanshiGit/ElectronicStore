cc33b41dabe1eec2398274928eea420ba9cfc46e
AmarSuryawanshiGit <suryawanshiamar0000@gmail.com>
2025-07-28 13:40:23 +0530

    Create Category Controller

--- ElectronicStore/src/main/java/com/lcwd/electronic/store/controller/CategoryController.java
+++ ElectronicStore/src/main/java/com/lcwd/electronic/store/controller/CategoryController.java
@@ -1,5 +1,66 @@
 package com.lcwd.electronic.store.controller;
 
+import com.lcwd.electronic.store.dtos.ApiResponse;
+import com.lcwd.electronic.store.dtos.CategoryDto;
+import com.lcwd.electronic.store.dtos.PageableResponce;
+import com.lcwd.electronic.store.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
 public class CategoryController {
+
+    private CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping("/createCategory")
+    public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto){
+        return new ResponseEntity<>(categoryService.createCategory(categoryDto), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/updateCategory/{categoryId}")
+    public ResponseEntity<CategoryDto> updateCategory(@RequestBody CategoryDto categoryDto,@PathVariable String categoryId){
+        return new ResponseEntity<>(categoryService.updateCategory(categoryDto,categoryId), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/deleteCategory/{categoryId}")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable String categoryId){
+
+        categoryService.deleteCategory(categoryId);
+
+        return new ResponseEntity<>(ApiResponse.builder()
+                .message("Category Deleted Succecfull !!!")
+                .success(true)
+                .status(HttpStatus.OK)
+                .build(),HttpStatus.OK);
+    }
+
+    @GetMapping("/getAllCategory")
+    public ResponseEntity<PageableResponce<CategoryDto>> getAllCategory(
+            @RequestParam(value = "pageNumber", defaultValue = "0", required = false) int pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = "title", required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir
+    ){
+        return new ResponseEntity<>(categoryService.getAllCategory(pageNumber,pageSize,sortBy,sortDir),HttpStatus.OK);
+    }
+
+    @GetMapping("/getCategory/{categoryId}")
+    public ResponseEntity<CategoryDto> getCategory(@PathVariable String categoryId){
+        return new ResponseEntity<>(categoryService.getCategory(categoryId),HttpStatus.OK);
+    }
+
+
+
+
 
 }
